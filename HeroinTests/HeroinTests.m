efb2f74607d07624b119202dd0e8a77ca186749f
Benson Chen <zjxucb@gmail.com>
2018-07-25 09:17:13 +0800

    creat new App

---
+++ HeroinTests/HeroinTests.m
@@ -0,0 +1,39 @@
+//
+//  HeroinTests.m
+//  HeroinTests
+//
+//  Created by Benson on 2018/7/25.
+//  Copyright © 2018年 Benson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HeroinTests : XCTestCase
+
+@end
+
+@implementation HeroinTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
